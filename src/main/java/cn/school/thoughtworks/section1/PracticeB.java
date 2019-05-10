195e67e436ec7780e882d69586634740d10528de
Narcissus <1169714441@qq.com>
2019-05-10 18:17:10 +0800

    完成cn.school.thoughtworks.section1.PracticeB,通过测试样例

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,10 @@
 package cn.school.thoughtworks.section1;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        return collection1.stream().filter(item -> collection2.stream().anyMatch(item1 -> item1.stream().anyMatch(item2 -> item.equals(item2)))).collect(Collectors.toList());
     }
 }
