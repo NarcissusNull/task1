c46f3f6131d2ef252480e1d1a257f13f0aaec63e
Narcissus <1169714441@qq.com>
2019-05-11 16:10:52 +0800

    完成cn.school.thoughtworks.section2.PracticeA,通过测试样例

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -2,11 +2,10 @@ package cn.school.thoughtworks.section2;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        return collection1.stream().collect(Collectors.groupingBy(item -> item, Collectors.reducing(0, e -> 1, Integer::sum)));
     }
 }
