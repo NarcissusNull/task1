0cee6ac0a5c2aee044faf201c18a0644fb77e773
Narcissus <1169714441@qq.com>
2019-05-12 16:47:42 +0800

    完成cn.school.thoughtworks.section3.PracticeD,通过测试样例

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -2,11 +2,16 @@ package cn.school.thoughtworks.section3;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeD {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
+        Map<String, Integer> result = collectionA.stream().collect(Collectors.groupingBy(item -> item.substring(0, 1), Collectors.reducing(0, PracticeD::getValue, Integer::sum)));
+        PracticeB practiceB = new PracticeB();
+        return practiceB.createUpdatedCollection(result, object);
+    }
 
-        return null;
+    private static Integer getValue(String e){
+        return e.length() == 1 ? 1 : Integer.valueOf(e.substring(2));
     }
 }
