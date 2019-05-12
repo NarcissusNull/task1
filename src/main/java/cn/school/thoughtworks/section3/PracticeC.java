b2b80534f4aab9808e81c2d9f367f6731d95be5a
Narcissus <1169714441@qq.com>
2019-05-12 16:45:27 +0800

    完成cn.school.thoughtworks.section3.PracticeC,通过测试样例

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -2,11 +2,12 @@ package cn.school.thoughtworks.section3;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeC {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> result = collectionA.stream().collect(Collectors.groupingBy(item -> item, Collectors.reducing(0, e -> 1, Integer::sum)));
+        PracticeB practiceB = new PracticeB();
+        return practiceB.createUpdatedCollection(result, object);
     }
 }
