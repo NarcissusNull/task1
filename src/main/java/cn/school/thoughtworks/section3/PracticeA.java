f8dedb670d538f3af7a3047f872ddf99faf231bf
Narcissus <1169714441@qq.com>
2019-05-12 16:05:31 +0800

    完成cn.school.thoughtworks.section3.PracticeA,通过测试样例

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -2,11 +2,12 @@ package cn.school.thoughtworks.section3;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeA {
-    Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+    Map<String, Integer> createUpdatedCollection(Map<String, Integer> collectionA, Map<String, List<String>> object) {
+        collectionA.entrySet().stream().forEach(e -> e.setValue(e.getValue() - (int) object.get("value").stream().filter(k -> e.getKey().equals(k)).count()));
+        return collectionA;
     }
+
 }
